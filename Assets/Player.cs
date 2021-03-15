030446dd60ba533889d1bd5f436f903c22970221
Filip Tarajko <filiptarajko@gmail.com>
2021-03-15 19:31:11 +0100

    add movement code

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -4,15 +4,52 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public Transform playerCamera;
+    public CharacterController characterController;
+    public float speed;
+    public bool isDoubleJumpAvailable;
+    public Vector3 moveVector;
+    public float gravitySpeed;
+    public float lastGrounded;
+
+    private void Update()
     {
-        
+        HandleGrounded();
+        HandleMovement();
     }
 
-    // Update is called once per frame
-    void Update()
+    void HandleGrounded()
     {
-        
+        lastGrounded += Time.deltaTime;
+        if (characterController.isGrounded)
+        {
+            isDoubleJumpAvailable = true;
+            if (lastGrounded > 0.1f)
+            {
+                gravitySpeed = -0.2f;
+            }
+            lastGrounded = 0;
+        }
+        else
+        {
+            gravitySpeed -= 10 * Time.deltaTime;
+        }
+    }
+    void HandleMovement()
+    {
+        moveVector = new Vector3(1 * Input.GetAxisRaw("Horizontal"), 0, 0);
+        moveVector.y = 0;
+        moveVector *= speed;
+        moveVector.y = gravitySpeed;
+        characterController.Move(moveVector * Time.deltaTime);
+        if (Input.GetKeyDown(KeyCode.Space) && lastGrounded < 0.15)
+        {
+            gravitySpeed = 10;
+        }
+        else if (Input.GetKeyDown(KeyCode.Space) && isDoubleJumpAvailable)
+        {
+            gravitySpeed = 10;
+            isDoubleJumpAvailable = false;
+        }
     }
 }
